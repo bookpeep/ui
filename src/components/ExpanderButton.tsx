1cfef4b24ba345a45cda1e1e3b7794f9e538d47e
theHasanas <haltabtabai@bookpeep.com>
2021-12-06 04:53:17 +0300

    Add expanderbutton componenet

---
+++ src/components/ExpanderButton.tsx
@@ -0,0 +1,25 @@
+import React, { memo } from 'react';
+import { Button, ButtonProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { ChevronDown } from '../icons';
+
+const endIcon = '& .MuiButton-endIcon';
+
+type ExpanderButtonProps = ButtonProps & {
+  expanded: boolean;
+};
+
+const ExpanderButton = styled((props: ExpanderButtonProps) => (
+  <Button endIcon={<ChevronDown />} {...props} />
+))<ExpanderButtonProps>(({ expanded, theme }) => ({
+  [endIcon]: {
+    transition: theme.transitions.create(['transform'], {
+      duration: theme.transitions.duration.shorter,
+    }),
+
+    color: theme.palette.primary.main,
+    transform: expanded ? 'rotate(180deg)' : 'rotate(0deg)',
+  },
+}));
+
+export default memo(ExpanderButton);
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as ExpanderButton } from './ExpanderButton';
