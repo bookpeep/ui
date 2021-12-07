f33146b975bf590b6025c09e93af2dd8d631b1ea
theHasanas <haltabtabai@bookpeep.com>
2021-12-07 09:44:24 +0300

    Add fonts

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,3 +1,8 @@
+import '@fontsource/quicksand/300.css';
+import '@fontsource/quicksand/400.css';
+import '@fontsource/quicksand/500.css';
+import '@fontsource/quicksand/600.css';
+import '@fontsource/quicksand/700.css';
 import { TypographyVariantsOptions } from '@mui/material';
 import palette from './palette';
 
