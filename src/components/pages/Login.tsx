7b8fe36b6bdf54174407f004cd7127ae24da6a14
theHasanas <haltabtabai@bookpeep.com>
2022-01-03 01:00:35 +0100

    Remove some console errors from login page

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,41 +1,42 @@
-import React from "react";
 import {
+  Box,
+  Button,
+  Card,
   Container,
   Grid,
   TextField,
-  Card,
-  Box,
   Typography,
-  Button,
 } from "@mui/material";
+
 import PasswordField from "../PasswordField";
+import React from "react";
 
 function Login() {
   return (
     <Container>
       <Grid container>
         <Grid item xs={12}>
-          <Box sx={loginLogoWrapper}>
+          <Box textAlign={"center"} sx={loginLogoWrapper}>
             <img src="https://placehold.co/68x28" alt="" />
           </Box>
           <Box sx={loginBoxWrapper}>
             <Card sx={loginCard}>
               <Grid container>
-                <Grid xs={12} mb="24px">
+                <Grid item xs={12} mb="24px">
                   <Typography variant="h3">Login with email</Typography>
                 </Grid>
-                <Grid xs={12} mb="24px">
+                <Grid item xs={12} mb="24px">
                   <TextField fullWidth label="Email" />
                 </Grid>
-                <Grid xs={12} mb="8px">
+                <Grid item xs={12} mb="8px">
                   <PasswordField fullWidth label="Password" />
                 </Grid>
-                <Grid xs={12}>
+                <Grid item xs={12}>
                   <Typography variant="h5" color="primary" component="a">
                     Forgot password?
                   </Typography>
                 </Grid>
-                <Grid xs={12} mt="30px">
+                <Grid item xs={12} mt="30px">
                   <Button fullWidth>Log in</Button>
                 </Grid>
               </Grid>
@@ -57,7 +58,6 @@ const loginCard = {
 };
 
 const loginLogoWrapper = {
-  textAlign: "center",
   marginBottom: "40px",
   marginTop: "40px",
 };
